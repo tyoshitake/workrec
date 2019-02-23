8a1ef8c517f5e54f7a3a9a4551dd61e1590fc6bd
tomoyuki.yoshitake <tyoshitake.dev@gmail.com>
2019-02-23 13:32:49 +0000

    Add login logout

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   root to: 'toppages#index'
   
   get 'signup', to: 'users#new'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  get 'logout', to: 'sessions#destroy'
   
   resources :users, only: [:show, :new, :create]
 end
