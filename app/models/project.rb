4d6cdc287d9ecf4567420c036c4b399102b8177c
tomoyuki.yoshitake <tyoshitake.dev@gmail.com>
2019-02-24 02:45:38 +0000

    Fix reference bug of reverses_of_work

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,6 +2,10 @@ class Project < ApplicationRecord
   validates :name, presence: true, length: { maximum: 100 },
                    uniqueness: { case_sensitive: true }
   
-  has_many :reverses_of_work, class_name: 'Work', foreign_key: 'user_id'
+  has_many :reverses_of_work, class_name: 'Work', foreign_key: 'project_id'
   has_many :members, through: :reverses_of_work, source: :user
+  
+  def get_members
+    @members = self.members.order(:id)
+  end
 end
